d0e6be18af0da72a02f6846abb17ab6a6ae10ea2
Bart Zed <bartzdrojewski@gmail.com>
2023-02-23 01:58:46 +0000

    todolist

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const todoItem_1 = require("./todoItem");
+const todoCollection_1 = require("./todoCollection");
+let todos = [
+    new todoItem_1.TodoItem(1, "Buy Flowers"), new todoItem_1.TodoItem(2, "Get Shoes"),
+    new todoItem_1.TodoItem(3, "collect Tickets"), new todoItem_1.TodoItem(4, "Call Kris", true)
+];
+let collection = new todoCollection_1.TodoCollection("Bart", todos);
 console.clear();
-console.log("Bart's First Project");
+console.log(`${collection.userName}'s Todo List`);
+let newId = collection.addTodo("Go for run");
+let todoItem = collection.getTodoById(newId);
+todoItem.printDetails();
+//collection.addTodo(todoItem);
